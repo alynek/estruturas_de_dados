e20af1388fe7d9d50329c22bbed11c41c878fe5b
Alyne Gois <alyne0x67@gmail.com>
2021-03-21 12:06:35 -0300

    Cria teste para o método Adiciona

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,7 +10,12 @@ namespace LinkedList
 
             lista.AdicionaNoComeco("Gandalf");
             lista.AdicionaNoComeco("Frodo");
+            System.Console.WriteLine(lista.Imprime());
 
+            lista.Adiciona("Sam");
+            System.Console.WriteLine(lista.Imprime());
+
+            lista.Adiciona(2, "Legolas");
             System.Console.WriteLine(lista.Imprime());
         }
     }
