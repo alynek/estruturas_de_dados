1919b56bcadb0bab61e9b69d21509528fba73346
Alyne Gois <alyne0x67@gmail.com>
2021-04-14 16:15:54 -0300

    Adiciona atributo Anterior, para aproveitar os benefícios de ter uma lista duplamente ligada

--- LinkedList/Celula.cs
+++ LinkedList/Celula.cs
@@ -3,6 +3,7 @@ namespace LinkedList
     public class Celula<T> where T : class
     {
         public Celula<T> Proxima{get; set;}
+        public Celula<T> Anterior{get; set;}
         public T Elemento{ get; private set; }
 
         public Celula(Celula<T> proxima, T elemento)
