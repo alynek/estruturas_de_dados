c6e1ef0b1a90f525ea03b1b316f90efb331477f3
Alyne Gois <alyne0x67@gmail.com>
2021-04-14 16:53:21 -0300

    Adiciona teste do método RemoveDoFim

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -15,11 +15,25 @@ namespace LinkedList
             lista.Adiciona("Sam");
             Console.WriteLine(lista.Imprime());
 
+            lista.Adiciona("Gollum");
+            Console.WriteLine(lista.Imprime());
+
+            lista.Adiciona("Bilbo");
+            Console.WriteLine(lista.Imprime());
+
             lista.Adiciona(2, "Legolas");
             Console.WriteLine(lista.Imprime());
 
             lista.RemoveDoComeco();
             Console.WriteLine(lista.Imprime());
+
+            Console.WriteLine(lista.Tamanho());
+            lista.RemoveDoFim();
+            Console.WriteLine(lista.Imprime());
+            
+
+            // lista.Remove(2);
+            // Console.WriteLine(lista.Imprime());
         }
     }
 }
