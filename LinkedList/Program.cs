5f5f24be6de8e19055c2d9fba1dab0e8bef75c77
Alyne Gois <alyne0x67@gmail.com>
2021-04-14 17:13:05 -0300

    Implementa teste dos métodos Contem, Pega, Remove

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -27,13 +27,19 @@ namespace LinkedList
             lista.RemoveDoComeco();
             Console.WriteLine(lista.Imprime());
 
-            Console.WriteLine(lista.Tamanho());
+            Console.WriteLine(lista.Tamanho()); //Imprime tamanho 5
+
             lista.RemoveDoFim();
             Console.WriteLine(lista.Imprime());
-            
 
-            // lista.Remove(2);
-            // Console.WriteLine(lista.Imprime());
+            lista.Remove(2);
+            Console.WriteLine(lista.Imprime());
+
+            Console.WriteLine(lista.Contem("Bilbo")); //False pois foi excluído com o método RemoveDoFim - linha 31
+            Console.WriteLine(lista.Contem("Gandalf")); //True
+
+            Console.WriteLine(lista.Pega(2)); //Retorna o elemento Gollum
+            Console.WriteLine(lista.Pega(5)); //Retorna um ArgumentException falando que a posição não existe
         }
     }
 }
